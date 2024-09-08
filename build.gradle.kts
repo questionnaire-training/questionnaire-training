8fc4a88f5e712946a718eb38fededc9a5f4677a2
Sergio del Amo <sergio.delamo@softamo.com>
2024-09-08 08:42:53 +0200

    use version catalogs & micronaut- platform/test

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0")
+    testImplementation(platform(libs.micronaut.platform))
+    testImplementation(libs.micronaut.test.junit5)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
 tasks.test {
